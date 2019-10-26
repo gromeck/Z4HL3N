d45594dc414e901aef1c46b598ceb7ae61e4137d
Christian Lorenz <christian.lorenz@gromeck.de>
2019-10-26 20:52:41 +0200

    added intro

--- pages/index.inc.php
+++ pages/index.inc.php
@@ -3,18 +3,23 @@
 **	Christian Lorenz
 */
 ?>
-<script language="JavaScript">
-function startTimer(seconds,id)
-{
-}
-
-function stopTimer()
-{
-}
-
-</script>
+<div class=intro>
+	<?php print __TITLE__ ?> ist ein einfachs Spiel, bei dem ausgeschriebene Zahlen
+	vorgegeben werden, die der Spieler ziffernweise eingeben muss.
+	<p>
+	Jeder korrekte Versuch wird gezählt.
+	<p>
+	Wieviele Zahlen schaffst du in <?php print __TIME_TO_PLAY__ ?> Sekunden?
+	<p>
+	Viel Spaß!
+</div>
 <?php
-
-	$seconds = 60;
-
+	/*
+	**	offer the options as buttons
+	*/
+	foreach ($pages as $pageid => $page) {
+		if (($page['title'] || $page['button']) && $page['show'] && !$page['admin']) {
+			html_button_href($page['button'] ? $page['button'] : $page['title'],'?page='.$pageid,-1);
+		}
+	}
 ?>	
